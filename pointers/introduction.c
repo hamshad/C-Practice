52a61f0c6c77eca7d3d9749141aaee0941bdf4b5
hamshad <hiromega18@gmail.com>
2024-09-25 17:59:32 +0530

    little alloc example

--- pointers/introduction.c
+++ pointers/introduction.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Person {
   char name[64];
@@ -12,7 +13,27 @@ void updateStruct(struct Person *p, int age) {
 
 int main(int argc, char *argv[])
 {
+
+  int x = 4; // 4
+  // English: integer named x is set to 4
+  int * pX = &x; // address of x
+  // English: integer pointer named pX is set to the address of x
+  int y = *pX; // 4 (dereferencing)
+  // English: integer y is set to the thing pointed to by pX
+
+
+  // Example:
   struct Person person = {"John", 20};
   updateStruct(&person, 100);
   printf("main: %d\n", person.age);
+
+
+  // Necessary need of pointers Example:
+  char *heapMemory = malloc(100);
+  if (NULL == heapMemory) {
+    printf("malloc failed!\n");
+  } 
+
+  return 0;
+
 }
